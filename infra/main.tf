d8634c9950f15b4b40c4093d4bb617a0427f7705
kjhappy77 <kjhappy77@gmail.com>
2026-06-22 15:02:53 +0900

    인프라: www.badasoft.com Route 53 A 레코드 추가

--- infra/main.tf
+++ infra/main.tf
@@ -820,6 +820,19 @@ resource "aws_route53_record" "root" {
   }
 }
 
+resource "aws_route53_record" "www" {
+  count   = var.domain_name != "" ? 1 : 0
+  zone_id = data.aws_route53_zone.main[0].zone_id
+  name    = "www.${var.domain_name}"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.main.dns_name
+    zone_id                = aws_lb.main.zone_id
+    evaluate_target_health = true
+  }
+}
+
 resource "aws_route53_record" "api" {
   count   = var.domain_name != "" ? 1 : 0
   zone_id = data.aws_route53_zone.main[0].zone_id
